bc141c3d89f3f3dcceb2103d76fb6d98e8e52a09
msearles25 <skaman4459@gmail.com>
2019-12-13 13:29:33 -0500

    fixed

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -14,11 +14,10 @@ const App = props => {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-        <button onClick={props.fetchSmurf}>Get the smurfs!</button>
+        
         <AddSmurfs />
+
+        <button onClick={props.fetchSmurf}>Get the smurfs!</button>
         <SmurfList />
       </div>
     );
